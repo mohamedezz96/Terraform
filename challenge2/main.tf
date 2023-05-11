b450c4153de35cec6128fa0b40d873449f3c6b7b
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2023-05-11 13:27:51 +0300

    Update main.tf

--- challenge2/main.tf
+++ challenge2/main.tf
@@ -9,7 +9,6 @@ resource "docker_image" "php-httpd-image" {
 }
 
 
-
 resource "docker_image" "mariadb-image" {
   name = "mariadb:challenge"
   build {
@@ -18,4 +17,61 @@ resource "docker_image" "mariadb-image" {
       challenge : "second"
     }
   }
+}
+
+resource "docker_network" "private_network" {
+  name = "my_network"
+  attachable = true
+  labels {
+        label = "challenge"
+        value = "second"
+ }
+}
+
+
+resource "docker_container" "php-httpd" {
+   name = "webserver"
+   image = "php-httpd:challenge"
+   hostname = "php-httpd"
+   networks_advanced {
+         name = "my_network"
+  }
+   ports {
+
+        internal = 80
+  }
+   labels {
+        label = "challenge"
+        value = "second"
+  }
+   volumes {
+
+        host_path = "/root/code/terraform-challenges/challenge2/lamp_stack/website_content/" 
+        container_path = "/var/www/html"
+  } 
+}
+
+
+
+
+
+resource "docker_container" "mariadb" {
+   name = "db"
+   image = "mariadb:challenge"
+   hostname = "db"
+   networks_advanced {
+         name = "my_network"
+  }
+   ports {
+
+        internal = 3306
+  }
+   labels {
+        label = "challenge"
+        value = "second"
+  }
+   env = [
+        "MYSQL_ROOT_PASSWORD=1234"
+        "MYSQL_DATABASE=simple-website"
+        ]
 }
