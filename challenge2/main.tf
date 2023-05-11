acb72116f9cc6d91c54d55694cd9b4b143c7d97c
Your Name <you@example.com>
2023-05-11 13:58:08 +0000

    terraform fmt

--- challenge2/main.tf
+++ challenge2/main.tf
@@ -20,37 +20,37 @@ resource "docker_image" "mariadb-image" {
 }
 
 resource "docker_network" "private_network" {
-  name = "my_network"
+  name       = "my_network"
   attachable = true
   labels {
-        label = "challenge"
-        value = "second"
- }
+    label = "challenge"
+    value = "second"
+  }
 }
 
 
 resource "docker_container" "php-httpd" {
-   name = "webserver"
-   image = "php-httpd:challenge"
-   hostname = "php-httpd"
-   networks_advanced {
-         name = "my_network"
+  name     = "webserver"
+  image    = "php-httpd:challenge"
+  hostname = "php-httpd"
+  networks_advanced {
+    name = "my_network"
   }
-   ports {
+  ports {
 
-        internal = 80
-        external = 80
-        ip = "0.0.0.0"
+    internal = 80
+    external = 80
+    ip       = "0.0.0.0"
   }
-   labels {
-        label = "challenge"
-        value = "second"
+  labels {
+    label = "challenge"
+    value = "second"
   }
-   volumes {
+  volumes {
 
-        host_path = "/root/code/terraform-challenges/challenge2/lamp_stack/website_content/" 
-        container_path = "/var/www/html"
-  } 
+    host_path      = "/root/code/terraform-challenges/challenge2/lamp_stack/website_content/"
+    container_path = "/var/www/html"
+  }
 }
 
 
@@ -60,30 +60,30 @@ resource "docker_volume" "mariadb_volume" {
 }
 
 resource "docker_container" "mariadb" {
-   name = "db"
-   image = "mariadb:challenge"
-   hostname = "db"
-   networks_advanced {
-         name = "my_network"
+  name     = "db"
+  image    = "mariadb:challenge"
+  hostname = "db"
+  networks_advanced {
+    name = "my_network"
   }
-   ports {
+  ports {
 
-        internal = 3306
-	external = 3306
-	ip = "0.0.0.0"
+    internal = 3306
+    external = 3306
+    ip       = "0.0.0.0"
   }
-   labels {
-        label = "challenge"
-        value = "second"
+  labels {
+    label = "challenge"
+    value = "second"
   }
-   env = [
-        "MYSQL_ROOT_PASSWORD=1234",
-        "MYSQL_DATABASE=simple-website"
-        ]
+  env = [
+    "MYSQL_ROOT_PASSWORD=1234",
+    "MYSQL_DATABASE=simple-website"
+  ]
 
   volumes {
-	volume_name = "mariadb-volume"
-	container_path = "/var/lib/mysql"
+    volume_name    = "mariadb-volume"
+    container_path = "/var/lib/mysql"
   }
 }
 
@@ -93,28 +93,28 @@ resource "docker_container" "mariadb" {
 
 
 resource "docker_container" "phpmyadmin" {
-   name = "db_dashboard"
-   image = "phpmyadmin/phpmyadmin"
-   hostname = "phpmyadmin"
-   networks_advanced {
-         name = "my_network"
+  name     = "db_dashboard"
+  image    = "phpmyadmin/phpmyadmin"
+  hostname = "phpmyadmin"
+  networks_advanced {
+    name = "my_network"
   }
-   ports {
+  ports {
 
-        internal = 80
-        external = 8081
-        ip = "0.0.0.0"
+    internal = 80
+    external = 8081
+    ip       = "0.0.0.0"
   }
-   labels {
-        label = "challenge"
-        value = "second"
+  labels {
+    label = "challenge"
+    value = "second"
   }
 
   volumes {
-        volume_name = "mariadb-volume"
-        container_path = "/var/lib/mysql"
+    volume_name    = "mariadb-volume"
+    container_path = "/var/lib/mysql"
   }
 
-  links = ["db"]
+  links      = ["db"]
   depends_on = [docker_container.mariadb]
 }
